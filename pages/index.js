d571a95a7ae306c55d99f2c6cd1339590ee430d2
placeparks <86215469+placeparks@users.noreply.github.com>
2023-03-22 20:37:41 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -364,7 +364,7 @@ export default function Home() {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="./cryptodevs/0.svg" />
+          <img className={styles.image} src="/devs.jpg" />
         </div>
       </div>
 
