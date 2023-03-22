b10be514475ed4e43261914f35315e2a3c91d967
placeparks <86215469+placeparks@users.noreply.github.com>
2023-03-22 22:10:14 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -364,7 +364,7 @@ export default function Home() {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="/devs.jpg" />
+          <img className={styles.image} src="/devs.png" />
         </div>
       </div>
 
